12f063aeba490c57fd89553e660389f02a494fc3
Doneal Piqué <135713732+DonealPique@users.noreply.github.com>
2025-02-17 14:45:46 +0100

    Update iframeLoader.js

--- docs/iframeLoader.js
+++ docs/iframeLoader.js
@@ -1,21 +1,26 @@
 document.addEventListener("DOMContentLoaded", function() {
     let gameContainer = document.createElement("div");
+    gameContainer.style.position = "absolute";
+    gameContainer.style.top = "0";
+    gameContainer.style.left = "0";
     gameContainer.style.width = "100%";
-    gameContainer.style.height = "calc(100vh - 50px)";
-    gameContainer.style.display = "flex";
-    gameContainer.style.justifyContent = "center";
-    gameContainer.style.alignItems = "center";
+    gameContainer.style.height = "100%";
+    gameContainer.style.overflow = "hidden";
+
     gameContainer.innerHTML = `
         <iframe id="gameFrame" src="https://donealpique.github.io/Avoid-The-Obstacles/" 
             style="width: 100%; height: 100%; border: none;" allowfullscreen></iframe>
     `;
+    
     document.body.appendChild(gameContainer);
     
+    console.log("Game IFrame Loaded!");
+
     window.addEventListener("resize", function() {
         let iframe = document.getElementById("gameFrame");
         if (iframe) {
             iframe.style.width = "100%";
-            iframe.style.height = "calc(100vh - 50px)"; 
+            iframe.style.height = "100%";
         }
     });
 
